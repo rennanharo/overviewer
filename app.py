854007b426634dab37a95587ddec1cc7b5d848eb
Rennan Haro <rennanharo@MacBook-Pro.local>
2020-07-14 10:30:02 -0300

    Setting up streamlit

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
+# Hiding the hamburguer menu
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 st.title("Hello streamlit!")
 st.subheader("Welcome to my first streamlit app!")
