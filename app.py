2a9405523d3d9d3cbe234b6ce03d862c5f8e0bbf
Rennan Haro <rennanharo@gmail.com>
2020-07-20 21:33:17 -0300

    Creating interactive elements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import GetOldTweets3 as got
 import pandas as pd
+import datetime, time
 
 # Hiding the hamburguer menu
 hide_streamlit_style = """
@@ -11,11 +12,14 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-st.title("Welcome to the Overviewer")
+st.title("Title")
+st.header("Header")
 st.subheader("Subheader")
-
+st.text("Pure text")
+st.markdown("- [ ] Markdown ")
 
 # TWITTER
+
 def get_tweets(search, location, startdate, enddate, maxtweet):
     
     tweetCriteria = got.manager.TweetCriteria().setQuerySearch(search)\
@@ -40,7 +44,45 @@ def get_tweets(search, location, startdate, enddate, maxtweet):
     
     return tweets_df
 
-#tweets = get_tweets(search.value), str(location.value), str(s_date.value), str(e_date.value), max_tweets.value)
+## Search --> Text box
+search = st.text_input("What are you searching for?", "Bolsonaro")
+
+## Location --> Text box (with map if possible)
+search = st.text_input("Where are you searching it for?", "Brazil")
+
+## Start date --> Date picker
+start_date = st.date_input("Select the start date", datetime.date(2019, 7, 30))
+
+## End date --> Date picker
+end_date = st.date_input("Select the end date", datetime.date(2019, 7, 30))
+
+## Max tweets --> Slider
+max_tweets = st.slider("What is the maximum number of Tweets you want?", 100, 5000, 2500, 100)
+
+## Run query button
+run_query = st.button("Run the query")
+if run_query:
+  ##TODO Call query function
+  #tweets = get_tweets(str(search.value), str(location.value), str(s_date.value), str(e_date.value), max_tweets.value)
+  pass
+
+## Download file button
+download_file = st.button("Download the CSV file")
+if download_file:
+  ##TODO Download file script
+  pass
+
+## Query status
+##TODO Keep it async and finish running when query complete
+with st.spinner("Wait..."):
+  time.sleep(15)
+st.success("Done!")
+
+##TODO DF HEAD PREVIEW
+st.dataframe(tweets.head())
+
+
+#
 
 #tweets.to_csv('tweets.csv', index = False)
 
--- todo.md
+++ todo.md
@@ -1,4 +1,19 @@
 # To-do
 
+## App
+
   [ ] Change app title
   [ ] Change app favicon
+  [ ] Sentiment analysis
+
+## Social
+
+### Twitter
+
+### Facebook
+
+### Instagram
+
+### Linkedin
+
+## News
