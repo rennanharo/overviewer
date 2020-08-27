face939c8d9a679dc0ee309da4e5b6d526d4fadc
Rennan Haro <rennanharo@gmail.com>
2020-08-26 23:52:07 -0300

    Instagram cleaning function implemented

---
+++ src/instagram/cleaner.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import json
+import csv
+
+# Call functions to populate the dictionary
+def clean_json(tag):
+    with open(f'./query_results/{tag}.json') as file:
+        data = json.load(file)
+    file.close()
+    
+    posts = range(len(data['GraphImages']))
+    
+    filtered = {
+    'likes': [],
+    'captions': [],
+    'comments': [],
+    'is_video': [],
+    'tags': []
+    }
+    
+    # Get likes
+    def get_likes(data, posts):
+        for post in posts:
+            edge_liked_by = data['GraphImages'][post]['edge_liked_by']
+            for item in edge_liked_by.values():
+                filtered['likes'].append(item)
+
+    # Get captions
+    def get_captions(data, posts):
+        for post in posts:
+            edge_media_to_caption = data['GraphImages'][post]['edge_media_to_caption']['edges'][0]['node']
+            for item in edge_media_to_caption.values():
+                filtered['captions'].append(item)
+
+    # Get comments
+    def get_comments(data, posts):
+        for post in posts:
+            media_comment = data['GraphImages'][post]['edge_media_to_comment']['data']
+            posts_range = range(len(data['GraphImages'][post]['edge_media_to_comment']['data']))
+            post_comments = []
+            for comment in posts_range:
+                post_comments.append(media_comment[comment]['text'])
+            filtered['comments'].append(post_comments)       
+
+    # Get is_video
+    def get_is_video(data, posts):
+        for post in posts:
+            is_video = data['GraphImages'][post]['is_video']
+            filtered['is_video'].append(is_video)
+
+    # Get tags
+    def get_tags(data, posts):
+        for post in posts:
+            tags = data['GraphImages'][post]['tags']
+            filtered['tags'].append(tags)
+
+    get_likes(data, posts)
+    get_captions(data, posts)
+    get_comments(data, posts)
+    get_is_video(data, posts)
+    get_tags(data, posts)
+
+    # Convert dictionary into dataframe
+    df = pd.DataFrame.from_dict(filtered)
+    return df
+
+    
--- src/instagram/instagram.py
+++ src/instagram/instagram.py
@@ -2,6 +2,8 @@ import os
 import pandas
 import streamlit as st
 
+from .cleaner import clean_json
+
 def render_instagram():
   st.header('Instagram')
   tag = st.sidebar.text_input('Which hashtag do you want to scrape comments from?')
@@ -11,4 +13,9 @@ def render_instagram():
   if run_query:
     os.system(f'instagram-scraper --media-types none --tag {tag} --maximum {maxp} --comments --retry-forever --destination ./query_results')
     st.text('doneeeeeee!!!')
+
+    insta_df = clean_json(tag)
+
+    st.text('doneeeeeee running the cleaning part')
+    st.dataframe(insta_df)
 
