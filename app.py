fc783abcfbd763298ddc9a31d82153eaae851ce5
Rennan Haro <rennanharo@gmail.com>
2020-09-17 16:21:55 -0300

    Css

--- app.py
+++ app.py
@@ -7,83 +7,21 @@ from views.home import render_home
 from views.twitter import render_twitter
 from views.instagram import render_instagram
 
-st.markdown(
-    """
-    <style>
-    [class^="st-b"]  {
-    }
-    .st-cs {
-    border-bottom-color: #0066cc
-    }
-    .st-cr {
-        border-top-color: #0066cc
-    }
-    .st-cq {
-        border-right-color: #0066cc
-    }
-    .st-cp {
-        border-left-color: #0066cc
-    }
-    header .decoration {
-        position: absolute;
-        top: 0;
-        right: 0;
-        left: 0;
-        height: 2px;
-        background-image: -webkit-gradient(linear,left top,right top,from(#003366),to(#0066cc));
-        background-image: -webkit-linear-gradient(left,#003366,#0066cc);
-        background-image: linear-gradient(90deg,#003366,#0066cc);
-        z-index: 120;
-    }
-    .btn-outline-info:not(:disabled):not(.disabled):focus, .btn-outline-info:not(:disabled):not(.disabled):hover, .btn-outline-primary:not(:disabled):not(.disabled):focus, .btn-outline-primary:not(:disabled):not(.disabled):hover, .btn-outline-secondary:not(:disabled):not(.disabled):focus, .btn-outline-secondary:not(:disabled):not(.disabled):hover {
-        background-color: #fff;
-        border-color: #0066cc;
-        color: #0066cc;
-    }
-    .btn-outline-info:not(:disabled):not(.disabled):focus, .btn-outline-primary:not(:disabled):not(.disabled):focus, .btn-outline-secondary:not(:disabled):not(.disabled):focus {
-        box-shadow: 0 0 0 0.2rem rgba(0,102,204,.5);
-    }
-    .st-fl {
-        linear-gradient(to right, rgb(0,102,204) 0%, rgb(0,102,204) 63.2653%, rgb(230, 234, 241) 63.2653%, rgb(230, 234, 241) 100%)
-    }
-    .st-f7:hover {
-        color: rgb(0,102,204);
-    }
-    .st-f6:hover {
-        border-color: rgb(0,102,204);
-    }
-    .st-ef:focus {
-        box-shadow: rgba(0,102,204, 0.5) 0px 0px 0px 0.2rem;
-    }
-    .st-f4:focus {
-        color: rgb(0,102,204);
-    }
-    .st-f3:focus {
-        border-color: rgb(0,102,204)
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+local_css("style.css")
 
-# Hiding the hamburguer menu
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+render_home()
 
 # Main text section
-pages = ["Home", "Twitter", "Instagram"]
-page = st.sidebar.selectbox("Select your page", pages)
+# pages = ["Home", "Twitter", "Instagram"]
+# page = st.sidebar.selectbox("Select your page", pages)
 
-
-#HomeTWITTER Code
-if page == 'Twitter':
-  render_twitter()
-if page == 'Instagram':
-  render_instagram()
-elif page == 'Home':
-  render_home()
+# #HomeTWITTER Code
+# if page == 'Twitter':
+#   render_twitter()
+# if page == 'Instagram':
+#   render_instagram()
+# elif page == 'Home':
+#   render_home()
--- views/home.py
+++ views/home.py
@@ -1,19 +1,21 @@
 import streamlit as st
 
+from models.app.img import img_to_bytes
+
 def render_home():
+  header_html = "<img src='data:image/png;base64,{}' class='home-logo'>".format(
+    img_to_bytes("assets/files/Logo_Fiat_Chrysler_Automobiles.png")
+  )
+  st.markdown(header_html, unsafe_allow_html=True)
+  
+  st.markdown("<h1 style='text-align: center;'>Explore what the world is saying about us.</h1>", unsafe_allow_html=True)
+
+
+  query = st.text_input('')
+  search = st.button('Search')
+
+  st.markdown('-'*7)
   st.markdown("""
-                  ## Welcome to The Overviewer.
-                  The Overviewer is an open source project to create a full-fledge tool to extract information from social media.
-
-                  Currently The Overviewer supports the following social media platforms:
-                  - Twitter
-                  - Instagram
-
                   You can check the [source code here.](https://github.com/rennanharo/overviewer)\n
                   Feel free to contribute with any suggestions or pull requests. Developed by [`Rennan Haro.`](https://github.com/rennanharo)
-            """)
-  st.markdown('-'*7)
-
-  # Tool selector
-  supported_tools = ["-","Twitter"]
-  st.markdown("### <~ Start by selecting your page in the `sidebar to the left`. ")
+              """)
